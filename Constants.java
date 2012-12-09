b988cb2bc1040df5b2969e7cebdbee0ac97e1e50
strattonw <stratton.wesley@gmail.com>
2012-12-09 02:53:42 -0500

    Updated Constants

--- Constants.java
+++ Constants.java
@@ -1,7 +1,6 @@
 package combatlogparser;
 
 public interface Constants {
-    public static final long FLAGS_ALL = -1;
     public static final long FLAGS_OBJECT_AFFILIATION_MASK = 15;
     public static final long FLAGS_OBJECT_AFFILIATION_MINE = 1;
     public static final long FLAGS_OBJECT_AFFILIATION_OUTSIDER = 8;
@@ -34,4 +33,50 @@ public interface Constants {
     public static final long FLAGS_OBJECT_TYPE_OBJECT = 16384;
     public static final long FLAGS_OBJECT_TYPE_PET = 4096;
     public static final long FLAGS_OBJECT_TYPE_PLAYER = 1024;
+
+    public static final int POWER_TYPE_UNKNOWN = -10;
+    public static final int POWER_TYPE_HEALTH = -2;
+    public static final int POWER_TYPE_MANA = 0;
+    public static final int POWER_TYPE_RAGE = 1;
+    public static final int POWER_TYPE_FOCUS = 2;
+    public static final int POWER_TYPE_ENERGY = 3;
+    //public static final int POWER_TYPE_PETHAPPINESS = 4;
+    public static final int POWER_TYPE_RUNES = 5;
+    public static final int POWER_TYPE_RUNIC_POWER = 6;
+    public static final int POWER_TYPE_SOUL_SHARD = 7;
+    public static final int POWER_TYPE_ECLIPSE_ENERGY = 8; //Solar > 0 && Lunar > 0
+    public static final int POWER_TYPE_HOLY_POWER = 9;
+    public static final int POWER_TYPE_ALTERNATE_POWER = 10;
+    public static final int POWER_TYPE_DARK_FORCE = 11;
+    public static final int POWER_TYPE_LIGHT_FORCE = 12;
+    public static final int POWER_TYPE_SHADOW_ORBS = 13;
+    public static final int POWER_TYPE_BURNING_EMBERS = 14;
+    public static final int POWER_TYPE_DEMONIC_FURY = 15;
+    
+    public static final int SCHOOL_NONE = 0;
+    public static final int SCHOOL_PHYSICAL = 1;
+    public static final int SCHOOL_HOLY = 2;
+    public static final int SCHOOL_FIRE = 4;
+    public static final int SCHOOL_NATURE = 8;
+    public static final int SCHOOL_FROST = 16;
+    public static final int SCHOOL_SHADOW = 32;
+    public static final int SCHOOL_ARCANE = 64;
+
+    public static final int GUID_MASK_PLAYER = 0;
+    public static final int GUID_MASK_WORLD_OBJECT = 1;
+    public static final int GUID_MASK_NPC = 3;
+    public static final int GUID_MASK_PERMANENT_PET = 4;
+    public static final int GUID_MASK_VEHICLE = 5;
+
+    public static final int CLASS_WARRIOR = 1;
+    public static final int CLASS_PALADIN = 2;
+    public static final int CLASS_HUNTER = 3;
+    public static final int CLASS_ROGUE = 4;
+    public static final int CLASS_PRIEST = 5;
+    public static final int CLASS_DEATH_KNIGHT = 6;
+    public static final int CLASS_SHAMAN = 7;
+    public static final int CLASS_MAGE = 8;
+    public static final int CLASS_WARLOCK = 9;
+    public static final int CLASS_MONK = 10;
+    public static final int CLASS_DRUID = 11;
 }
